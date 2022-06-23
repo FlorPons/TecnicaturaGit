7e54b8077aa78d9051956af0f4cc1ddbcd701c53
Florencia Pons <floritoloza@gmail.com>
2022-06-23 15:44:01 -0300

    Ingresamos nuestra carpeta Jaca al commit

--- Java/Leccion1/HolaMundo/src/HolaMundo.java
+++ Java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -174,16 +174,16 @@ public class HolaMundo {
          /*var edad = Integer.parseInt("20"); 
          System.out.println("edad = " + (edad + 1));
          var valorPI = Double.parseDouble("3.1416");
-         System.out.println("valorPI = " + valorPI);*/
+         System.out.println("valorPI = " + valorPI);
          
          //Pedir un valor
          var entrada = new Scanner (System.in);
-         /*System.out.println("Digite su edad: ");
+         System.out.println("Digite su edad: ");
          edad = Integer.parseInt(entrada.nextLine());
          System.out.println("edad = " + edad);*/
          
          //Conversion de tipos primitivos en Java Parte 2
-         var edadTexto = String.valueOf(10);
+         /*var edadTexto = String.valueOf(10);
          System.out.println("edadTexto = " + edadTexto);
          
          var fraseChar = "programadores".charAt(4);
@@ -192,6 +192,7 @@ public class HolaMundo {
          System.out.println("Digite un caracter: ");
          fraseChar = entrada.nextLine().charAt(0);
          System.out.println("fraseChar = " + fraseChar);
+         */
          
          
                  
